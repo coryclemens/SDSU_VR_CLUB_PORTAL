b0e7645474637f6e658d2d985124e1997ef6d2bd
coryclemens <cordaciousclems@gmail.com>
2019-09-13 00:41:00 -0700

    tell me what you think animate on scroll infopage

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll(".nav-links li");
+const links = document.querySelectorAll('.nav-links li');
+var unity = document.querySelector('.unity');
+var landing = document.querySelector('.landing');
+var h1 = document.querySelector('.landing h1');
+
+landing.addEventListener("mouseover", () =>{
+    unity.classList.toggle("hover");
+    h1.classList.toggle("reverse-hover");
+});
+
+landing.addEventListener("mouseout", () =>{
+    unity.classList.toggle("hover");
+    h1.classList.toggle("reverse-hover");
+});
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
@@ -8,6 +21,7 @@ hamburger.addEventListener("click", () => {
         link.classList.toggle("fade");
     });
 });
+
 
 function facebook(){
     newUrl = "https://www.facebook.com/VRClubSDSU";
